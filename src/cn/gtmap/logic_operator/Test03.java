1693003fdf4848eb844919bb3528aef573465090
TWM <15152806231@163.com>
2020-01-06 19:00:16 +0800

    取反

---
+++ src/cn/gtmap/logic_operator/Test03.java
@@ -0,0 +1,13 @@
+package cn.gtmap.logic_operator;
+
+public class Test03 {
+	/**
+	 * 3¡¢È¡·´
+	 */
+	public static void main(String[] args) {
+		boolean b = true;
+		System.out.println(b);
+		System.out.println(!b);
+	}
+
+}
