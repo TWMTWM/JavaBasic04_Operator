0a2a74b0f4e1f6baeabfcf2ba5e3673bd9b7a70e
TWM <15152806231@163.com>
2020-01-06 12:20:32 +0800

    基本操作符

---
+++ src/cn/gtamp/operator/Test01.java
@@ -0,0 +1,23 @@
+package cn.gtamp.operator;
+
+/**
+ * 算数操作符
+ * @author Administrator
+ *
+ */
+public class Test01 {
+	
+	public static void main(String[] args) {
+		/**
+		 * 1、基本算数操作符：+ - * /
+		 */
+		int a = 10;
+		int b = 5;
+		int c = a + b;
+		int d = a - b;
+		int e = a * b;
+		int f = a / b;
+		
+	}
+
+}
