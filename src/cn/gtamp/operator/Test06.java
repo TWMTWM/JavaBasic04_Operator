d0960e6bb98991c09544fe148af0be725bd0787a
TWM <15152806231@163.com>
2020-01-06 12:41:05 +0800

    自增自减

---
+++ src/cn/gtamp/operator/Test06.java
@@ -0,0 +1,14 @@
+package cn.gtamp.operator;
+
+public class Test06 {
+	/**
+	 * 
+	 * 6、自增、自减
+	 */
+	public static void main(String[] args) {
+		int i = 5;
+		i++;
+		System.out.println(i);//输出为6
+	}
+
+}
