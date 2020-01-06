10f274f6a6526bca4f06e749baca5d071230eee7
TWM <15152806231@163.com>
2020-01-06 12:39:28 +0800

    取模

---
+++ src/cn/gtamp/operator/Test05.java
@@ -0,0 +1,14 @@
+package cn.gtamp.operator;
+
+public class Test05 {
+	/**
+	 * 
+	 * 5、%取模
+	 */
+	public static void main(String[] args) {
+		int i = 5;
+		int j = 2;
+		System.out.println(i % j);//输出为1
+	}
+
+}
