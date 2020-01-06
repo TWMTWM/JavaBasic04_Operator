d34e4647f4f76a8db978deb3edca8c27b85103a8
TWM <15152806231@163.com>
2020-01-06 19:35:35 +0800

    对本身进行运算、赋值

---
+++ src/cn/gtmap/assignment_operator/Test02.java
@@ -0,0 +1,18 @@
+package cn.gtmap.assignment_operator;
+
+public class Test02 {
+	/**
+	 * 对本身进行运算、并赋值
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int i = 3;
+		i+=2;
+		System.out.println(i);
+		
+		int j = 2;
+		j = j + 2;
+		System.out.println(j);
+	}
+
+}
